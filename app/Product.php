965b6c9dd8b8f7b9479cd6525e3dcc51b9743cb4
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-10-17 18:16:55 +0200

    AAAAAAAAAAAAAAAAAA

--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,11 @@ class Product extends Model
     {
     	return $this->hasMany('App\Type');
     }
+
+    public function getPriceAttribute($value)
+    {
+        $discount=$value*($this->discount/100); //Kortingineuro's
+        $final_price=$value-$discount; //Haalkortingafvanprijs
+        return number_format($final_price,2); //Zorgaltijdvoor2decimalen
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,9 +8,12 @@
 				<img src="{{ url($product->image ?? 'img/placeholder.jpg') }}" alt="{{ $product->title }}" class="rounded">
 				<div class="product-body">
 					<div>
-						<h5 class="product-title"><span>{{ $product->title }}</span><em>&euro;{{ $product->price }}</em></h5>
+						<h5 class="product-title"><span>{{ $product->title }}</span><em>&euro;{{ $product->getPriceAttribute($product->price) }}</em></h5>
 						@unless(empty($product->description))
 							<p>{{ $product->description }}</p>
+						@endunless
+						@unless($product->discount == 0)
+							<p class="discount">Nu <strong>{{$product->discount . "%"}}</strong> korting! Originele prijs: &euro;{{$product->price}}</p>
 						@endunless
 					</div>
 					<button class="btn btn-primary">Meer info &amp; bestellen</button>
